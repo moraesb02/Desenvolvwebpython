f94e68b49081a5a9c58aaf3d55f4a65af690e0a0
moraesb02 <luckmoraes02@gmail.com>
2024-05-23 20:19:12 -0300

    algumas informações sobre estudo de API´S

---
+++ main.py
@@ -0,0 +1,20 @@
+#tipos de APIS
+
+#Restful
+#referen-se a API´ com padrões HTTP(json)
+#usado para operações CRUD(post,get,delete,put,patch)
+#GET PARA LEITURA
+#POST PARA CRIAÇÃO DE INFORMAÇÃO
+#PUT/PATCH PARA EDITAR AS INFORMAÇÕES
+
+
+
+#APISOAP
+#referen-se a API´ com padrões XML
+#mais burocrático para enviar e receber informações, restull é mais facil, mas é mais padronizado as saidas e entradas
+#independent de linguagem de transporte
+#suporte para operações complexas
+#mais definições de segurança
+
+#FACILITADOR PARA APIS
+#GRAPHQL
